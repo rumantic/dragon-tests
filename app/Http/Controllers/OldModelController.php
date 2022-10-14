491c8a11ba3a54265c67155da064c14fd32d4350
Kondin Dmitriy <dkondin@gmail.com>
2022-10-14 13:20:52 +0700

    тестовый прогон по трем моделям

--- app/Http/Controllers/OldModelController.php
+++ app/Http/Controllers/OldModelController.php
@@ -2,33 +2,58 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\Client;
-use Sitebill\Dragon\app\Models\AnyModel;
 use Sitebill\Dragon\Eloquent\Casts\SelectBox;
+use Sitebill\Dragon\Helpers\DragonHelper;
 
 class OldModelController extends Controller
 {
 
     public function load_data () {
-        $anyModel = new \Sitebill\Dragon\app\Models\AnyModel();
-        //$anyModel->setTable('data');
-        $anyModel->setTable('client');
-        //$anyModel->setTable('migrations');
-
-
-        //$record = $anyModel->where('client_id', 18)->first();
-        //$record = $anyModel->take(3)->get();
+        $anyModel = DragonHelper::getDynamicModel('client');
+        echo 'client model key = '.$anyModel->getKeyName().'<br>';
         $record = $anyModel->where('client_id', 3607)->first();
-        $record->fio = 'Test Model';
-        //$record->update();
 
-        $client = new Client();
-        $record = $client->where('client_id', 3607)->first();
-        $record->fio = 'Иванов Иван';
+        echo $record->fio->value.'<br>';
+        $record->fio = 'Test Model '.microtime();
         $record->save();
-        //dd($record);
 
-        return response()->json($record);
+        $record = $anyModel->where('client_id', 3607)->first();
+        echo $record->fio->value.'<br>';
+        $record->fio = 'Test Model '.microtime();
+        $record->save();
+
+        $dataModel = DragonHelper::getDynamicModel('data');
+        echo 'data model key = '.$dataModel->getKeyName().'<br>';
+        $record = $dataModel->where('id', 50072479)->first();
+        //$data_id = $record->id->value;
+        echo $record->id->value.' '.$record->price->value.'<br>';
+        $record->price->value += 1;
+        $record->save();
+
+        $versionModel = DragonHelper::getDynamicModel('version');
+        $versionModel->create([
+            'version' => 1,
+            'code' => uniqid(),
+            'name' => 'Simple test',
+        ]);
+        $lastVersion = $versionModel->orderby('version_id', 'desc')->first();
+        echo $lastVersion->code->value.'<br>';
+        $version_array = $versionModel->orderby('version_id', 'desc')->take(1)->get()->toArray();
+        echo '<pre>';
+        print_r($version_array);
+        echo '</pre>';
+
+
+        //$record = $dataModel->where('id', $data_id)->first();
+        //echo $record->id->value.' '.$record->price->value.'<br>';
+
+
+
+
+        exit;
+
+
+        //return response()->json($record);
         //return $record->toJson();
     }
 
